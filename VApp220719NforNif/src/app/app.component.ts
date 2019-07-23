3b57446d2223b9be38a611e1ee1ab4f19dad2fea
leonmatias2015 <leonmatias1991@gmail.com>
2019-07-22 22:49:09 -0500

    save changes

--- VApp220719NforNif/src/app/app.component.ts
+++ VApp220719NforNif/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'VApp220719NforNif';
+  nombre = 'Roslin';
+  apellido = 'leon';
 }
