8d4d921d98cc12efa86741e82444f0da31316abf
leon matias <leonmatias1991@gmail.com>
2019-08-10 17:54:46 -0500

    change

--- VApp100819Clase04Cibertec/src/app/views/products/list-product/list-product.component.ts
+++ VApp100819Clase04Cibertec/src/app/views/products/list-product/list-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵConsole } from '@angular/core';
 import Product from '../product.model';
 import { SortPipe } from '../shared/pipes/sort.pipe';
 import { ProductsService } from '../shared/services/products.service';
@@ -18,8 +18,17 @@ export class ListProductComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.allProducts = this.productService.getProducts();
+    this.productService.getProducts().subscribe((data:any) => {      
+      this.allProducts = data;
+      //      this.allProducts = data;
+    });
   }
+  /**
+   * 
+   * .subscribe((data: any) => {
+         this.nuevasCanciones = data;
+      });
+   */
 
   onSort(value: string) {
     this.sortPipe.transform(
@@ -29,8 +38,11 @@ export class ListProductComponent implements OnInit {
   }
 
   onRemove(id: number) {
-    this.productService.deleteProduct(id);
-    this.allProducts = this.allProducts.filter(prod =>  prod.id !== id);
+    this.productService.deleteProduct(id).subscribe((response)=>{
+      console.log(response);
+      this.allProducts = this.allProducts.filter(prod=> prod.id != id);
+    });
+    
   }
 
 }
--- VApp100819Clase04Cibertec/src/app/views/products/shared/services/products.service.ts
+++ VApp100819Clase04Cibertec/src/app/views/products/shared/services/products.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
 
-import { products } from '../../../../mocks/products';
+//import { products } from '../../../../mocks/products';
 import Product from '../../product.model';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient} from "@angular/common/http";
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable()
 export class ProductsService {
 
-    products: Product[] = products;
+    private api:string = environment.api;
+    private endpoint:string = 'products';
 
-    constructor(private http: HttpClientModule) {}
+    
 
-    getProducts() {
-        return this.products;
+    constructor(private http: HttpClient) {}
+
+    getProducts(){
+        const URL:string = `${this.api}/${this.endpoint}`;
+        return this.http.get(URL);        
     }
 
     getProduct(id: number) {
-        const prod = this.products.filter(prod => prod.id === id); // []
-        return prod[0];
+        //const prod = this.products.filter(prod => prod.id === id); // []
+        //return prod[0];
     }
 
     deleteProduct(id: number) {
-        this.products = this.products.filter(prod => prod.id !== id);
+        const URL:string = `${this.api}/${this.endpoint}`;
+        return this.http.delete(`${URL}/${id}`);
+        //this.products = this.products.filter(prod => prod.id !== id);
     }
 }
