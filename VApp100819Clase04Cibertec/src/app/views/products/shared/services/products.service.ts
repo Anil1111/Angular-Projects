07957e2254c6268cdd0028306d753869d4556d36
leon matias <leonmatias1991@gmail.com>
2019-08-10 17:01:52 -0500

    changes

--- VApp100819Clase04Cibertec/src/app/views/products/shared/services/products.service.ts
+++ VApp100819Clase04Cibertec/src/app/views/products/shared/services/products.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 
 import { products } from '../../../../mocks/products';
 import Product from '../../product.model';
+import { HttpClientModule } from "@angular/common/http";
 
 @Injectable()
 export class ProductsService {
 
     products: Product[] = products;
 
-    constructor() {}
+    constructor(private http: HttpClientModule) {}
 
     getProducts() {
         return this.products;
